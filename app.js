75cdb5629771149ad1757374818b888445147b38
justinzhang <justinz@andrew.cmu.edu>
2022-01-07 17:44:42 -0500

    first commit of nodejs hbs framework

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+var exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+var handlebars = exphbs.create({
+    helpers: {
+        sayHello: function () { alert("Hello World") },
+        getStringifiedJson: function (value) {
+            return JSON.stringify(value);
+        }
+    },
+    defaultLayout: 'index',
+    partialsDir: ['views/partials/']
+});
+
+
+//Sets handlebars configurations
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+res.render('main');
+});
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
